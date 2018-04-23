4157ebf1070f865d8eff128c5f08f1d390cab046
Aritra <aritra.dhar7@gmail.com>
2018-04-23 12:10:10 +0200

    Automated SHIT

--- IntegriScreenServer/src/MainServer.java
+++ IntegriScreenServer/src/MainServer.java
@@ -2,25 +2,40 @@
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * Servlet implementation class MainServer
  */
 @WebServlet("/MainServer")
+@MultipartConfig
+
 public class MainServer extends HttpServlet {
+
+	public static String location = "/home/dhara/tomcat/static/data";
+	public static String generatedLocation = "/home/dhara/tomcat/static/generated";
+	
+	
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -30,10 +45,10 @@ public class MainServer extends HttpServlet {
 	public MainServer() {
 		super();
 		//create the list of json files
-		File[] currentFiles = new File(".").listFiles();
+		File[] currentFiles = new File(location).listFiles();
 		for(File file : currentFiles)
 		{
-			System.out.println(file);
+			//System.out.println(file);
 			if(file.getName().contains(".json"))
 				fileString.add(file.getName().replaceAll(".json", ""));
 		}
@@ -45,6 +60,15 @@ public class MainServer extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String pageId = request.getParameter("page");
+
+
+		String file = request.getParameter("file"); // Retrieves <input type="text" name="description">
+		Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
+		//String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+		//InputStream fileContent = filePart.getInputStream();
+
+		System.out.println(file);
+
 
 		if(pageId == null)
 			response.getWriter().append("ERROR!");
@@ -63,11 +87,89 @@ public class MainServer extends HttpServlet {
 	}
 
 	/**
+	 * @throws IOException 
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		//String flag =  request.getParameter("flag");
+
+		//if(flag.equals("upload"))
+		//	System.out.println("UPLOAD!!");
+
+
+		Part filePart = null;
+		try {
+			filePart = request.getPart("file");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // Retrieves <input type="file" name="file">
+		String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+
+		if(fileName != null)
+		{
+			InputStream fileContent = null;
+			try {
+				fileContent = filePart.getInputStream();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			StringWriter writer = new StringWriter();
+			try {
+				IOUtils.copy(fileContent, writer, StandardCharsets.UTF_8);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String fileData = writer.toString();
+
+			System.out.println("FILENAME:" + fileName + " Received");
+
+			FileWriter fw;
+			try {
+				fw = new FileWriter(location+ "/" + fileName);
+				fw.append(fileData);
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				response.getWriter().append(fileName + " uploaded");
+				response.sendRedirect("/upload.html");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		//System.out.println("File data : " + fileData);
+		else
+		{
+			System.out.println("Generate requiest received");
+			
+			File[] currentFiles = new File(location).listFiles();
+			for(File file : currentFiles)
+			{
+				//System.out.println(file);
+				if(file.getName().contains(".json"))
+					fileString.add(file.getName().replaceAll(".json", ""));
+			}
+			
+			StringBuffer logs = new StringBuffer();
+			for(String pageFileName:fileString)
+			{
+				String out = PageGen.pageGen(pageFileName);
+				System.out.println("Generated : " + pageFileName);
+				logs.append(out + "\n");
+			}
+			
+			response.getWriter().append(logs.toString());
+		}
 	}
 
 }
---
+++ IntegriScreenServer/src/PageGen.java
@@ -0,0 +1,172 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+//*************************************************************************************
+//*********************************************************************************** *
+//author Aritra Dhar 																* *
+//Beautiful Life Specialist														  	* *
+//ETH Zurich													   				    * *
+//Zurich, Switzerland															    * *
+//--------------------------------------------------------------------------------- * * 
+///////////////////////////////////////////////// 									* *
+//This program is meant to do world domination... 									* *
+///////////////////////////////////////////////// 									* *
+//*********************************************************************************** *
+//*************************************************************************************
+
+/**
+ * @author Aritra
+ *
+ */
+public class PageGen {
+	public static void main(String[] args) throws IOException {
+		// This will load email.json as input and generate email.html as output
+		pageGen("email_1080_960");
+		pageGen("email_1920_1080");
+	}
+
+	public static String pageGen(String pageFileName) throws IOException
+	{
+		String jsonData = new String(Files.readAllBytes(new File(pageFileName + ".json").toPath()), StandardCharsets.UTF_8);
+		String htmlFile = new String(Files.readAllBytes(new File("template.txt").toPath()), StandardCharsets.UTF_8);
+		
+		System.out.println("Opening File: " + pageFileName + ".html");
+		
+
+		FileWriter fw = new FileWriter(MainServer.generatedLocation + pageFileName + ".html");
+		FileWriter fwu = new FileWriter(MainServer.generatedLocation + pageFileName + "_unicorn.html"); // "_unicorn files are those where the borders are colored"
+		FileWriter jsonSpecsFile = new FileWriter(MainServer.generatedLocation + pageFileName + "_specs.json");
+		
+		JSONObject jObject = new JSONObject(jsonData);
+		String pageName = jObject.getString("page");
+		String ratioString = jObject.getString("ratio");
+		String ratioHeight = ratioString.split(":")[0];
+		int heightInt = Integer.parseInt(ratioHeight);
+		String ratioWidth = ratioString.split(":")[1];
+		int widthInt = Integer.parseInt(ratioWidth);
+		String form_action = jObject.getString("form_action");
+		
+		String border_thickness = jObject.getString("border_thickness");
+		
+		System.out.println(htmlFile.contains("!!page_name!!"));
+		htmlFile = htmlFile.replaceAll("!!page_name!!", pageName).replaceAll("!!height!!", ratioHeight).replaceAll("!!width!!", ratioWidth);
+		
+	
+		JSONArray elements = jObject.getJSONArray("elements");
+		
+		
+		/*
+		 * Subject:<br>
+  		<input type="text" name="subject" value="Bla Bla">
+  		<br>
+
+		 */
+		int divCounter = 1;
+		StringBuffer elementHtmlString = new StringBuffer();
+		
+		String vspace = jObject.getString("vspace");
+		int vspaceInt = Integer.parseInt(vspace);
+		
+		//container div
+		elementHtmlString.append("<div style=\"height:" + vspace + "vh; width:" + String.valueOf(Math.round((double)vspaceInt * heightInt / widthInt)) + "vh; margin: 0 auto; position:relative;\" id=\"frameBox\">\n");
+		// green border overlay
+		elementHtmlString.append("<div style=\"border:" + border_thickness + "vh solid #00ff00; height:100%; width:100%; margin: 0 auto; position:absolute;box-sizing:border-box;\" id=\"greenBox\"></div>\n");
+		
+		int titleCounter = 0;
+		
+		for(int i = 0; i < elements.length(); i++)
+		{
+			JSONObject inObject = elements.getJSONObject(i);
+			String id = inObject.getString("id");
+			String type = inObject.getString("type");
+			//String editable = inObject.getString("editable");
+			String initialValue = inObject.getString("initialvalue");
+			String ulc_x = inObject.getString("ulc_x");
+			String ulc_y = inObject.getString("ulc_y");
+			
+			String width = inObject.getString("width");
+			String height = inObject.getString("height");
+			
+			System.out.println(id);
+			
+					
+			if(type.equalsIgnoreCase("title"))
+			{
+				if(titleCounter == 1)
+				{
+					System.err.println("Error! More than 1 HTML title. Exiting");
+					System.exit(1);
+				}
+				
+				elementHtmlString.append("<h2 style=\"width:" + width + "%;left:" + ulc_x +"%;top:" + ulc_y +"vh; position:absolute\">" + initialValue + "</h2>\n");
+				
+				//form action
+				elementHtmlString.append("<form action=\""+ form_action + "\">");
+				titleCounter++;
+			}
+			
+			else if(type.equalsIgnoreCase("textarea"))
+			{
+				elementHtmlString.append("<textarea style=\"left:" + ulc_x + "%;top:" + ulc_y + "vh;position:absolute;height:" + height + "vh;width:" + width + "%;\">" + initialValue + "</textarea>\n");
+			
+			}
+			
+			else if(type.equalsIgnoreCase("textfield"))
+			{
+
+				elementHtmlString.append("<input type=" + type + " name =" + id + " value=\"" + initialValue 
+						+ "\" style=\"width:" + width + "%;left:" + ulc_x + "%;top:" + ulc_y + "vh;position:absolute\">\n");
+			}
+			
+			else if(type.equalsIgnoreCase("label"))
+			{
+
+				elementHtmlString.append("<label style=\"width:" + width + "%;left:" + ulc_x + "%;top:" + ulc_y + "vh;position:absolute;\">" + initialValue + "</label>\n");
+			}
+			
+			divCounter++;
+			
+			
+		    Double vert_mul = 100.0 / vspaceInt;
+	        // Convert vspace and height from vh to percentage relative to the green border
+			Double ulc_y_d = Double.parseDouble(ulc_y) * vert_mul;
+			Double height_d = Double.parseDouble(height) * vert_mul;
+			
+			
+			inObject.put("ulc_y", String.valueOf(ulc_y_d));
+			inObject.put("height", String.valueOf(height_d));
+
+		}
+		elementHtmlString.append("</form>\n</div>");
+
+		
+		// ----- Generate the main .html file		
+		htmlFile = htmlFile.replaceAll("!!body!!", elementHtmlString.toString());
+		// Write out both files
+		fw.write(htmlFile);    fw.close();
+		
+		// ----- Generate the _unicorn.html file		
+		// Add the additional styling to make the UI elements colored
+		htmlFile = htmlFile.replaceAll("<!--place-for-unicorn-styling-->",
+				"label {background: #ffbdbd;} "
+				+ "h1, h2, h3, h4, h5, h6 {background: #c9c9ff;}"
+				+ "input {background: #e1f7d5;}");		
+		fwu.write(htmlFile);  fwu.close();
+		
+		
+		// --- Generate the _specs.json file
+		jsonSpecsFile.write(jObject.toString(2)); jsonSpecsFile.close();
+		
+		System.out.println("Page generated");
+		
+		return "Generated HTML => " + MainServer.generatedLocation + pageFileName + ".html" + "\n" + "Generated Uniocorn => " + MainServer.generatedLocation + pageFileName + "_unicorn.html";
+		
+	}
+
+}
