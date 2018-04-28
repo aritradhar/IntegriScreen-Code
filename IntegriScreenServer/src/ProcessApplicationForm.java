b50933831ff511f5983dc17971e7c61c79f3c5b5
Ivo Sluganovic <ivo.sluganovic@gmail.com>
2018-04-28 12:36:48 +0200

    Simplified the Application Form server logic

--- IntegriScreenServer/src/ProcessApplicationForm.java
+++ IntegriScreenServer/src/ProcessApplicationForm.java
@@ -1,22 +1,8 @@
-//*************************************************************************************
-//*********************************************************************************** *
-//author Aritra Dhar 																* *
-//PhD Researcher																  	* *
-//ETH Zurich													   				    * *
-//Zurich, Switzerland															    * *
-//--------------------------------------------------------------------------------- * * 
-///////////////////////////////////////////////// 									* *
-//This program is meant to do world domination... 									* *
-///////////////////////////////////////////////// 									* *
-//*********************************************************************************** *
-//*************************************************************************************
-
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -30,14 +16,10 @@ import org.json.JSONObject;
 
 
 
-/**
- * @author Aritra
- *
- */
 public class ProcessApplicationForm {
-	
-	public static volatile Map<String, HashMap<String, String>> data = new ConcurrentHashMap<>();
-	
+
+	public static volatile Map<String, HashMap<String, String>> allBrowserResponses = new ConcurrentHashMap<>();
+
 	public static void processApplicationForm(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
 		String pageID = request.getParameter("page_id");
@@ -53,116 +35,75 @@ public class ProcessApplicationForm {
 			KVPair.put(key, request.getParameter(key));
 			System.out.println("Key: " + key + " | value: " + request.getParameter(key));
 		}
-		System.out.println("--------");
-		
-		
-		data.put(pageID, KVPair);
-		
-		response.getWriter().write("Response recorded");
+
+
+		allBrowserResponses.put(pageID, KVPair);
+
+		response.getWriter().write("Response recorded: " + KVPair.toString());
 		response.flushBuffer();
 	}
 	
 	public static void processApplicationFormPhone(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
-	
-		StringBuffer responseBuffer = new StringBuffer();
+
 		String jsonString = IOUtils.toString(request.getInputStream(), StandardCharsets.UTF_8);
-		JSONObject jObject = new JSONObject(jsonString);
+		JSONObject phoneDataJson = new JSONObject(jsonString);
+		String page_id = phoneDataJson.getString("page_id");
+
 		
-		System.out.println("JSON String:" + jObject.toString(1));
-		String page_id = jObject.getString("page_id");
+		JSONObject outJson = new JSONObject();
+		JSONArray failArray = new JSONArray();
+			
 		
-		boolean match = true;
-		
-		if(!data.containsKey(page_id))
+		if(!allBrowserResponses.containsKey(page_id))
 		{
-			response.getWriter().write("{\"response\" : \"__NULL__\"}");
-			response.flushBuffer();
-			return;
+			outJson.put("response", "__NULL__");
+			outJson.put("info", "No data for page " + page_id + " submitted from the browser yet.");	
 		}
 		else
 		{
-			JSONObject responseObject = new JSONObject();
-			
-			Set<String> keySet = jObject.keySet();
-			Iterator<String> keys = keySet.iterator();
-			HashMap<String, String> params = data.get(page_id);
-			
-			HashMap<String, String> jsonParams = new HashMap<>();
-			
-			for(String key:keySet)
-				jsonParams.put(key, jObject.getString(key));
-			
-			
-			JSONArray complainArray = new JSONArray();
-			while(keys.hasNext())
-			{
-				String key = keys.next();
-				if(key.equals("page_id") || key.equals("page_type"))
-					continue;
-				
-				if(params.containsKey(key))
-				{
-					if(!jObject.getString(key).equals(params.get(key)))
-					{
-						JSONObject complainObject = new JSONObject();
-						complainObject.put("key", key);
-						complainObject.put("phone_value", jObject.getString(key));
-						complainObject.put("browser_value", params.get(key));
-						complainArray.put(complainObject);
-						//responseBuffer.append("Phone contains : |" + key + "| : |" + jObject.getString(key) + "| browser response contains |" +  key + "| : |" + params.get(key) + "\n");
-						match = false;
-					}
-				}
-				else
-				{
-					JSONObject complainObject = new JSONObject();
-					complainObject.put("key", key);
-					complainObject.put("phone_value", jObject.getString(key));
-					complainObject.put("browser_value", "__NULL__");
-					complainArray.put(complainObject);
-					//responseBuffer.append("Phone contains : |" + key + "| but missing in browser response\n");
-					match = false;
-				}
-			}
+			Set<String> allKeys = new HashSet<String>(phoneDataJson.keySet());
+			HashMap<String, String> phoneResponse = new HashMap<String, String>();
+			for(String key:allKeys) phoneResponse.put(key, phoneDataJson.getString(key));
 
+			HashMap<String, String> browserResponse = allBrowserResponses.get(page_id);	
 			
-			for(String browserKey:params.keySet())
+			// Create a union of all keys
+			allKeys.addAll(browserResponse.keySet());
+						
+			
+			// Go through all the keys that I know of
+			for(String key: allKeys)
 			{
-				if(browserKey.equals("page_type"))
+				if (key.equals("page_id") || key.equals("page_type"))
 					continue;
-				if(!jsonParams.containsKey(browserKey))
-				{
-					JSONObject complainObject = new JSONObject();
-					complainObject.put("key", browserKey);
-					complainObject.put("phone_value", "__NULL__");
-					complainObject.put("browser_value", params.get(browserKey));
-					complainArray.put(complainObject);
-					
-					//responseBuffer.append("Browser response contains : |" + browserKey + "| but missing in phone response\n");
-					match = false;
-				}
-			}
-			
-			
-			if(match)
-			{
-				responseBuffer.append("{\"response\":\"match\"}");
-				System.out.println("Page data removed from map");
-				//data.remove(page_id);
+
+				String phoneVal = "__NULL__"; // = phoneDataJson.getString(key);
+				String browserVal = "__NULL__"; // = browserResponse.get(key);
+
+				if (phoneResponse.containsKey(key)) phoneVal = phoneResponse.get(key);
+				if (browserResponse.containsKey(key)) browserVal = browserResponse.get(key);
 				
-				response.getWriter().write(responseBuffer.toString());
-				response.flushBuffer();
+				if(!phoneVal.equals(browserVal))
+				{
+					JSONObject failJSON = new JSONObject();
+					failJSON.put("key", key);
+					failJSON.put("phone", phoneVal);
+					failJSON.put("browser", browserVal);
+					failArray.put(failJSON);
+				}
+ 			}
+
+			if(failArray.length() == 0) {
+				outJson.put("response", "match");
+			} else {
+				outJson.put("response", "nomatch");
+				outJson.put("diffs", failArray);
 			}
-			else
-			{
-				responseObject.put("response", "notmatch");
-				responseObject.put("reasons", complainArray);
-				response.getWriter().write(responseObject.toString(1));
-				response.flushBuffer();
-			}
-			
-			
 		}
+		
+		// Output the generated JSON
+		response.getWriter().write(outJson.toString());
+		response.flushBuffer();
 	}
 }
