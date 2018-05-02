262b6554bcd9ea78791b0d5e88a0551d5409a5ad
Daniele <daniele.lain3@gmail.com>
2018-05-02 16:40:28 +0200

    Better ocr_tester.js

--- IntegriScreenServer/data/js/ocr_tester.js
+++ IntegriScreenServer/data/js/ocr_tester.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
     
     var url = new URL(window.location.href);
     var testing = url.searchParams.get("ocr_testing");
+    var time = url.searchParams.get("time");
+    var limit = url.searchParams.get("time");
     
     if (testing == null) return;
+    if (time == null) time = 10000;
+    if (limit == null) limit = 109
                   
     setTimeout(function() {
-        window.location.href = window.location.href.replace(/Random_\d.html/, `Random_${parseInt(testing)+1}.html`).replace(/ocr_testing=\d/, `ocr_testing=${parseInt(testing)+1}`);}, 10000);
+        target = parseInt(testing);
+        limit = parseInt(limit);
+        if (target == limit) window.location.href = window.location.href.replace(/Random_\d.html/, 'stop.html');
+        window.location.href = window.location.href.replace(/Random_\d.html/, `Random_${target+1}.html`).replace(/ocr_testing=\d/, `ocr_testing=${target+1}`);
+    }, parseInt(time));
                   
 });
