3df22c9150079189a5beeb9191189c39da0f44c8
Daniele <daniele.lain3@gmail.com>
2018-06-19 16:00:08 +0200

    new attack modes

--- IntegriScreenServer/data/js/attack.js
+++ IntegriScreenServer/data/js/attack.js
@@ -9,7 +9,7 @@ function parallel(attack) {
     log(`binded-${$(targets[0]).attr('name')}-${$(targets[0]).val()}`);
     $(targets[0]).keydown(function() {
         key_count += 1;
-        if (key_count == 3) {
+        if (key_count === 3) {
             log(`target-${$(targets[1]).attr('name')}-${$(targets[1]).val()}`);
             attack($(targets[1]));    // chosen by fair dice roll, guaranteed to be random
         }
@@ -29,10 +29,53 @@ function inactive(attack) {
     });
 }
 
+function same(attack) {
+    let target =  _.sample($("textarea,input[type='textfield'],input[type='text']"));
+    const trigger = Math.floor(Math.random() * 3) + 4;
+    log(`binded-${$(target).attr('name')}-${$(target).val()}`);
+    $(target).keydown(function() {
+        key_count += 1;
+        if (key_count === trigger) {
+            attack($(target));    // chosen by fair dice roll, guaranteed to be random
+        }
+    });
+}
+
+keystroke_buffer = [];
+time_cheating = 0;
+function change_focus(attack) {
+    targets = _.sample($("textarea,input[type='textfield'],input[type='text']"), 2);
+    log(`binded-${$(targets[0]).attr('name')}-${$(targets[0]).val()}`);
+    $(targets[0]).keydown(function() {
+        key_count += 1;
+        if (key_count === 3) {
+            log(`target-${$(targets[1]).attr('name')}-${$(targets[1]).val()}`);
+            $(targets[1]).keydown(function(e) {
+                e.preventDefault();
+                console.log(e);
+                keystroke_buffer.push(e.key);
+            });
+            $(targets[1]).focus();
+            typed_chars = attack($(targets[1]));    // chosen by fair dice roll, guaranteed to be random
+            setTimeout(function() {
+                $(targets[0]).focus();
+                keystroke_buffer.forEach(function(char, idx) {
+                    setTimeout(function() {
+                        $(targets[0]).keydown(char);
+                    }, 80 * (idx + 1))
+                })
+            }, typed_chars * 120 + time_cheating)
+
+        }
+    });
+}
+
 attack_modes = {
     'inactive': inactive,
-    'parallel': parallel
-}
+    'parallel': parallel,
+    'same': same,
+    'change_focus': change_focus
+};
 
 alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
 
@@ -77,31 +120,68 @@ function remove_char(target) {
     log(`remove_char-${r_char}-${t_char}`);
 }
 
+function add_bunch(target) {
+    value = [...target.val()];
+    howmany = Math.floor(Math.random() * 2) + 2;
+    t_char = Math.floor(Math.random() * value.length);
+    n_char = _.sample(alphabet, howmany);
+    n_char.forEach(function(chr, idx) {
+        setTimeout(function(){
+            console.log(target, chr, idx);
+            value.splice(t_char+idx, 0, chr);
+            target.val(value.join(''));
+        }, 120 * (idx + 1))
+    });
+    log(`add_bunch-${n_char.join('')}-${t_char}`);
+    return n_char.length
+}
 
-attacks = {
-    'replace_char': replace_char,
-    'flip_chars': flip_chars,
-    'add_char': add_char,
-    'remove_char': remove_char
+function replace_bunch(target) {
+    value = [...target.val()];
+    howmany = Math.floor(Math.random() * 2) + 2;
+    t_char_start = Math.floor(Math.random() * (value.length - howmany));
+    s_chars = value.slice(t_char_start, t_char_start + howmany);
+    new_values = _.sample(alphabet, howmany);
+    new_values.forEach(function(chr, idx) {
+        setTimeout(function(){
+            value[t_char_start + idx] = chr;
+            target.val(value.join(''));
+        }, 120 * (idx + 1))
+    });
+    // value = value.slice(0, t_char_start).concat(new_values, value.slice(t_char_start + howmany));
+    // target.val(value.join(''));
+    // attack - original char - position - new char
+    log(`replace_bunch-${s_chars.join('')}-${t_char_start}-${new_values.join('')}`);
+    return new_values.length
 }
 
 
+attacks = {
+    'replace_char': replace_char,
+    'replace_bunch': replace_bunch,
+    'flip_chars': flip_chars,
+    'add_char': add_char,
+    'add_bunch': add_bunch,
+    'remove_char': remove_char,
+};
+
+
 $(document).ready(function() {
-    
-    var url = new URL(window.location.href);
-    var mode = url.searchParams.get("atk_mode");
-    var attack = url.searchParams.get("atk_type");
-    
+
+    let url = new URL(window.location.href);
+    let mode = url.searchParams.get("atk_mode");
+    let attack = url.searchParams.get("atk_type");
+
     if (mode == null) return;
-    if (attack == 'random' || attack == null) attack = _.sample(Object.keys(attacks));
+    if (attack === 'random' || attack == null) attack = _.sample(Object.keys(attacks));
     
-    $('#frameBox form').append(`<input type='hidden' name='atk_mode' value='${mode}'>`);
-    $('#frameBox form').append(`<input type='hidden' name='atk_type' value='${attack}'>`);
+    $('#frameBox form').append(`<input type='hidden' name='atk_mode' value='${mode}'>`)
+        .append(`<input type='hidden' name='atk_type' value='${attack}'>`);
     
     form_elems = $('input');
-    text_elems_count = form_elems.toArray().reduce(function(a, b) { return a + (b.type == 'text'); }, 0) + $("textarea").length;
+    text_elems_count = form_elems.toArray().reduce(function(a, b) { return a + (b.type === 'text'); }, 0) + $("textarea").length;
     
-    if (mode == 'parallel' && text_elems_count < 2) log("Less than 2 text inputs -- not attacking");  
+    if (mode === 'parallel' && text_elems_count < 2) log("Less than 2 text inputs -- not attacking");
                   
     attack_modes[mode](attacks[attack]);
 
